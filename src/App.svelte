ca48ac444d3799ca82b83f787b0bf8ba9cd5fe33
Abel Masila <abelmasila@gmail.com>
2021-10-14 02:33:00 +0300

    final form

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import FeedbackList from './components/FeedbackList.svelte';
   import FeedbackStats from './components/FeedbackStats.svelte';
+  import FeedbackForm from './components/FeedbackForm.svelte';
   let feedback = [
     {
       id: 1,
@@ -30,6 +31,7 @@
 </script>
 
 <main class="container">
+  <FeedbackForm />
   <FeedbackStats {count} {average} />
   <FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
 </main>
---
+++ src/components/FeedbackForm.svelte
@@ -0,0 +1,74 @@
+<script>
+  import Card from './Card.svelte';
+  import Button from './Button.svelte';
+
+  let text = '';
+  let btnDisabled = true;
+  let min = 10;
+  let message;
+
+  const handleInput = () => {
+    if (text.trim().length <= min) {
+      message = `Text must be at least ${min} characters`;
+      btnDisabled = true;
+    } else {
+      message = '';
+      btnDisabled = false;
+    }
+  };
+</script>
+
+<Card>
+  <header>
+    <h2>How would you rate your service with us?</h2>
+  </header>
+  <form>
+    <!-- Rating Select -->
+    <div class="input-group">
+      <input
+        placeholder="Tell us something that keeps you coming back"
+        on:input={handleInput}
+        bind:value={text}
+      />
+      <Button type="submit" disabled={btnDisabled}>Send</Button>
+    </div>
+    {#if message}
+      <div class="message">
+        {message}
+      </div>
+    {/if}
+  </form>
+</Card>
+
+<style>
+  header {
+    max-width: 400px;
+    margin: auto;
+  }
+  header h2 {
+    font-size: 22px;
+    font-weight: 600;
+    text-align: center;
+  }
+  .input-group {
+    display: flex;
+    flex-direction: row;
+    border: 1px solid #ccc;
+    padding: 8px 10px;
+    border-radius: 8px;
+    margin-top: 15px;
+  }
+  input {
+    flex-grow: 2;
+    border: none;
+    font-size: 16px;
+  }
+  input:focus {
+    outline: none;
+  }
+  .message {
+    padding-top: 10px;
+    text-align: center;
+    color: rebeccapurple;
+  }
+</style>
